7979490917dd3eb6c2ff2dc1cbb6c824082f5bc9
cailey90 <chris.ailey90@gmail.com>
2022-07-19 16:17:08 +0100

    implemented user login and jwt authentication, created Home.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import Signup from './user/Signup'
+import Login from './user/Login'
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import Axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import Home from './home/Home'
 
 
 export default function App() {
+
+  useEffect(() => {
+    let token = localStorage.getItem("token")
+
+    if(token != null){
+      let user = jwt_decode(token)
+      console.log("USER: ", user.user.name)
+
+      if(user){
+        setIsAuth(true)
+        setUser(true)
+      }
+      else if (!user){
+        localStorage.removeItem("token");
+        setIsAuth(false)
+      }
+    }
+  }, [])
 
   const [isAuth, setIsAuth] = useState(false)
 
@@ -21,15 +42,53 @@ export default function App() {
     })
   }
 
+  const loginHandler = (cred) => {
+    console.log(cred)
+    Axios.post("auth/login", cred)
+    .then(response => {
+      console.log(response.data.token)
+      if(response.data.token != null){
+        localStorage.setItem("token", response.data.token);
+        let user = jwt_decode(response.data.token)
+        setIsAuth(true)
+        setUser(user)
+        console.log("User successfully logged in.")
+      }
+    })
+  }
+
+  const onLogoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setIsAuth(false)
+    setUser(null)
+    console.log("User successfully logged out.")
+  }
+
   return (
     <div>
       <Router>
         <nav>
-          <Link to="/signup">Sign Up</Link> &nbsp;
+          { isAuth ? (
+            <div>
+              {user ? `Welcome, ${user.user.name}!` : "null"} &nbsp;
+              <Link to="/">Home</Link> &nbsp;
+              <Link to="/logout" onClick={onLogoutHandler}>Log Out</Link> &nbsp;
+            </div>
+
+          ):(
+            <div>
+              <Link to="/">Home</Link> &nbsp;
+              <Link to="/signup">Sign Up</Link> &nbsp;
+              <Link to="/login">Log In</Link> &nbsp;
+          </div>
+          )}
         </nav>
         <div>
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/signup" element={<Signup register={registerHandler} />} />
+            <Route path="/login" element={<Login login={loginHandler} />} />
           </Routes>
         </div>
       </Router>
---
+++ src/user/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import {Container, Form, Button} from 'react-bootstrap'
+
+export default function Login(props) {
+
+    const [newUser, setNewUser] = useState({})
+
+    const handleChange = (event) => {
+        const user = {...newUser}
+        user[event.target.name] = event.target.value
+        console.log(user)
+        setNewUser(user)
+    }
+
+    const loginHandler = () => {
+        props.login(newUser)
+    }
+
+  return (
+    <div>
+        <h1>Log In</h1>
+        <Container>
+            <Form.Group>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control name="emailAddress" onChange={handleChange} />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" type="password" onChange={handleChange} />
+            </Form.Group>
+
+            <Button variant="primary" onClick={loginHandler}>Log In</Button>
+        </Container>
+    </div>
+  )
+}
