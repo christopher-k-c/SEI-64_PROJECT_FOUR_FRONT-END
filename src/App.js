f5df33d5aa4dea3331ea8459ea6dbd9f7b0e76be
christopher-k-c <chriscareyk@gmail.com>
2022-07-19 17:01:57 +0100

    Product FE Card/Routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react'
 import Signup from './user/Signup'
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import Axios from 'axios'
+import ProductList from './product/ProductList'
+import Product from './product/Product'
 
 
 export default function App() {
 
   const [isAuth, setIsAuth] = useState(false)
-
   const [user, setUser] = useState({})
+  // 
+  const [products, setProducts] = useState([])
   
   const registerHandler = (user) => {
     console.log("Made it this far")
@@ -21,20 +24,34 @@ export default function App() {
     })
   }
 
+  const allProducts = products.map((products, index) => (
+
+    <div key={index}>
+
+        <Product  {...products} />
+
+    </div>
+
+  ))
+
   return (
     <div>
       <Router>
         <nav>
           <Link to="/signup">Sign Up</Link> &nbsp;
+          <Link to="/index">Products</Link> &nbsp;
+          
+          
         </nav>
         <div>
           <Routes>
             <Route path="/signup" element={<Signup register={registerHandler} />} />
+            <Route path="/index" element={<ProductList allProducts={allProducts} setProducts={setProducts}/>} />
           </Routes>
         </div>
       </Router>
-
-
+      {/* <ProductList /> */}
+    
 
     </div>
   )
