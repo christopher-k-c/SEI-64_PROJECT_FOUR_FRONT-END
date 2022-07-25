6f780d8ab018980a0d57b245eec4a38e13e5a885
christopher-k-c <chriscareyk@gmail.com>
2022-07-25 18:18:22 +0100

    carousel

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,17 +6,32 @@ import NewsLetter from './NewsLetter';
 
 export default function Footer(props) {
   return (
+
+    <>
+    <div className="clear"></div>
     <div className="footer-container fixed-bottom d-flex justify-content-center m-auto" >
 
 
+<div className="footer"> 
+      <div className="footer-heading footer-2">
+        <h2>About Us</h2>
 
+      </div>
+      <div className="footer-heading footer-3">
+        <h2>Products</h2>
 
+      </div>
+      <div className="footer-heading footer-3">
+        <h2>Social Media</h2>
+
+      </div>
+      
       <NewsLetter addNewsletterEmail={props.addNewsletterEmail}/>
-    
+      </div>
     
     </div>
 
-
+    </>
   )
 }
 
